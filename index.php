e5d7c350112f2103e71e97252a929404f278738a
Scott Wales <scott.wales@unimelb.edu.au>
2012-04-12 12:04:02 +1000

    Basic structure

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en-GB">
+    <head>
+        <title>Layout test for CAP interface</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+        <link type="text/css" href="layout.css" rel="stylesheet">
+
+        <!--Content from Google CDN http://code.google.com/apis/libraries/devguide.html -->
+        <link type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/themes/base/jquery-ui.css" rel="stylesheet">
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"> </script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"> </script>
+        <script type="text/javascript"> 
+        
+        </script>
+    </head>
+    <body>
+
+    </body>
+</html>
+
+        
