b409f5d13ee49541354bb78ad06457a8a8f53aee
Scott Wales <scott.wales@unimelb.edu.au>
2012-05-23 10:40:08 +1000

    Test section metadata read

--- test/model/model.php
+++ test/model/model.php
@@ -1,5 +1,6 @@
 <?php
 require_once('model/model.php');
+require_once('model/metadata.php');
 
 class ModelTest extends PHPUnit_Framework_TestCase {
     public function testDefaultConstructor() {
@@ -7,6 +8,25 @@ class ModelTest extends PHPUnit_Framework_TestCase {
 
         $this->assertEquals(is_array($model->sections),true);
     } 
+
+    public function testReadSectionMetadata(){
+        $metadata = <<<EOF
+[section]
+name=section name
+tooltip=tooltip
+script variable = variable
+always enable =true 
+
+EOF;
+        $model = new Model();
+        ReadSectionMetadata($model,$metadata);
+
+        $this->assertTrue(array_key_exists('section',$model->sections));
+        $this->assertEquals($model->sections['section']->Name(),'section name');
+        $this->assertEquals($model->sections['section']->Tooltip(),'tooltip');
+        $this->assertEquals($model->sections['section']->ScriptVariable(),'variable');
+        $this->assertTrue($model->sections['section']->IsEnabled());
+    }
 }
 
 ?>
